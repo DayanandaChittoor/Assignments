9220b3ba265a797fcdb3ebe86641b8f3be1f4c87
DayanandaChittoor <seenu.daya5@gmail.com>
2021-08-03 19:49:08 +0530

    feat: Adding Part Time Wage

--- Employee_Pay_Roll/Employee_Pay_Roll/Program.cs
+++ Employee_Pay_Roll/Employee_Pay_Roll/Program.cs
@@ -9,14 +9,21 @@ namespace Employee_Pay_Roll
             //DailyWage Calculation
             int perHourWage = 1000;
             int HOURS_WORKED = 8;
+            int PART_TIME_HOURS = 4;
             int dailyWage = 0;
             Random rand = new Random();
-            int present = rand.Next(0, 2);
-            Console.WriteLine(present);
-            if (present == 1)
+            int workStatus = rand.Next(0, 3);
+            Console.WriteLine(workStatus);
+            ///Ctrl + R+R
+            if (workStatus == 1)
             {
                 Console.WriteLine("Employee is Present!");
                 dailyWage = perHourWage * HOURS_WORKED;
+            }
+            else if (workStatus == 2)
+            {
+                Console.WriteLine("Employee worked for part time");
+                dailyWage = perHourWage * PART_TIME_HOURS;
             }
             else
             {
