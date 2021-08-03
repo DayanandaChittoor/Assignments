9769914899f4f441c61f4f77256484562daeb2e3
DayanandaChittoor <seenu.daya5@gmail.com>
2021-08-03 19:34:03 +0530

    feat: Daily Wage Calculation

--- Employee_Pay_Roll/Employee_Pay_Roll/Program.cs
+++ Employee_Pay_Roll/Employee_Pay_Roll/Program.cs
@@ -6,16 +6,23 @@ namespace Employee_Pay_Roll
     {
         static void Main(string[] args)
         {
+            //DailyWage Calculation
+            int perHourWage = 1000;
+            int HOURS_WORKED = 8;
+            int dailyWage = 0;
             Random rand = new Random();
             int present = rand.Next(0, 2);
+            Console.WriteLine(present);
             if (present == 1)
             {
                 Console.WriteLine("Employee is Present!");
+                dailyWage = perHourWage * HOURS_WORKED;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
             }
+            Console.WriteLine("Daily Wage of Employee:" + dailyWage);
         }
     }
 }
