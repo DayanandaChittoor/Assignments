9cbf53fc0d4d625068a4ad3ffd6831b6218588dd
DayanandaChittoor <seenu.daya5@gmail.com>
2021-08-03 19:31:08 +0530

    Adding: code

---
+++ Employee_Pay_Roll/Employee_Pay_Roll/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Employee_Pay_Roll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int present = rand.Next(0, 2);
+            if (present == 1)
+            {
+                Console.WriteLine("Employee is Present!");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
